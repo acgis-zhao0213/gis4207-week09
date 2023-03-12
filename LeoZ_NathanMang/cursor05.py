4122e3589d384239dc88d1e4a88fe29cb8b4defa
Leo Zhao <zhao0213@agongquinlive.com>
2023-03-12 16:33:15 -0400

    Finish 05&06

--- LeoZ_NathanMang/cursor05.py
+++ LeoZ_NathanMang/cursor05.py
@@ -1,27 +1,30 @@
-import sys
-def main():
-    global arcpy
-    if len(sys.argv) != 2:
-        print('Usage: cursor02.py <prov_abbrev>')
-        sys.exit()
-    # Check prov_abbrev is ON, QC, ...
-    prov_name = ['ON','QC','NB','NS']
-    if sys.argv[1] in prov_name:
-        import arcpy
-        ws = r'..\..\..\..\data\Canada\Canada.gdb'
-        prov = sys.argv[1].upper()
-        fields=['NAME','PROV','SHAPE@X','SHAPE@Y']
-        field = arcpy.AddFieldDelimiters(ws, 'PROV')
-       
-        where_clause = f"{field} = '{prov}'"
-    
-        arcpy.env.workspace = ws
-        fc = 'MajorCities'
-        
-        with arcpy.da.SearchCursor(fc, fields, where_clause) as cursor:
-            for row in cursor:
-    
-                print (row) 
+from lxml import etree
+import arcpy
+import os
+# Check prov_abbrev is ON, QC, ...
+prov_name = ['ON','QC','NB','NS']
+ws = r'..\..\..\..\data\Canada\Canada.gdb'
+fields=['NAME','PROV','SHAPE@X','SHAPE@Y','UTM_MAP']
+arcpy.env.workspace = ws
+fc = 'MajorCities'
+# create KML document
+kml_doc = etree.Element("kml", xmlns="http://www.opengis.net/kml/2.2")
+doc = etree.SubElement(kml_doc, "Document")
+with arcpy.da.SearchCursor(fc, fields) as cursor:
+    for row in cursor:
+        name = row[0] + ", " + row[1]
+        url = "http://www.canmaps.com/topo/nts50/map/{}.htm".format(row[4])
+        coordinates = "{}".format(row[2],row[3]) # replace with actual coordinates
+        placemark = etree.SubElement(doc, "Placemark")
+        etree.SubElement(placemark, "name").text = name
+        etree.SubElement(placemark, "description").text = url
+        point = etree.SubElement(placemark, "Point")
+        etree.SubElement(point, "coordinates").text = coordinates
 
-if __name__ == "__main__":
-    main()
+parent_dir = os.path.abspath(os.path.join(os.getcwd(), ".."))
+output_path = os.path.join(parent_dir, "output","Cities.kml")
+print(parent_dir)
+print(output_path) 
+# write KML document to file
+with open(output_path, "wb") as f:
+    f.write(etree.tostring(kml_doc, pretty_print=True))
